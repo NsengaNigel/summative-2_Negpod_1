a61b42979f4421abbf02d0d4487e6c9d0ad21f9d
IrakozeLoraine <l.irakoze2@alustudent.com>
2024-02-21 01:39:06 -0800

    refactor: receive user input

--- main.sh
+++ main.sh
@@ -10,4 +10,15 @@ echo "3. Delete Student Record"
 echo "4. Update Student Record"
 echo "5. Exit"
 
+read -p "Select from the drop down" Choice
+
+case $Choice in
+1) create_student_record ;;
+2) view_all_students ;;
+3) delete_student_record ;;
+4) update_student_record ;;
+5) echo " Exiting the ALU registration system, Thank you!"; exit 0 ;;
+*) echo "Invalid choice, Please enter a valid choice." ;;
+esac
+
 done
