88868e5614938f532dce9408eea7fd60cfbbdbe5
JDMUHIRWA <j.hareriman@alustudent.com>
2024-02-21 02:19:33 -0800

    Information about student name

--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@ Student_record="students-list_1023.txt"
 #creating a student record
 create_student_record() { echo "Enter student details:"
 	read -p "Student ID:" Student_ID
+	read -p "Student name:" Student_name
 
 #Updating Student record 01 by ID
 update_student_record() { 
