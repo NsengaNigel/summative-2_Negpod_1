2b2bb200ea301bad5ab21d262cf93a258f790025
IrakozeLoraine <l.irakoze2@alustudent.com>
2024-02-21 01:34:08 -0800

    feat: add menu

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Home
+
+while true; do
+echo "ALU registration system"
+echo "1. Create Student Record"
+echo "2. View All Students"
+echo "3. Delete Student Record"
+echo "4. Update Student Record"
+echo "5. Exit"
+
+done
