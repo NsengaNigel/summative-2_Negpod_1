1c6aeee2d608f4848d14e6fd7aed399825478302
Nsenga Nigel <nsenganigel@gmail.com>
2024-02-21 01:48:25 -0800

    updating_student-record 01

--- main.sh
+++ main.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#Updating Student record 01 by ID
+update_student_record() { 
+    read -p "Enter your Student ID" update_ID
+    if [ -f students-list_1023.txt ] && [[ $(grep -c "$update_ID" students-list_1023.txt) -gt 0 ]]; then
+	    read -p "Student name:" student_name
+	    read -p "Student age:" student_age
+	    read -p "Student email:" student_email
+
+	    sed -i "/^$update_ID,/c\\$update_ID,$student_name,$student_age,$student_email" students-list_1023.txt
+
+	    echo "the user has been updated"
+
+    else
+	    echo "Student not found"
+    fi
+}
+
 #Home
 
 while true; do
