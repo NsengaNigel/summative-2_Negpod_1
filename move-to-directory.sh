f43b6bf5fe91d169bc351dae2d1a9144cc5428c1
IrakozeLoraine <l.irakoze2@alustudent.com>
2024-02-21 02:45:52 -0800

    ref: move to directory

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 directory="negpod_1-q1"
+
+# Check if the directory exist or else we create it
+
+if [ ! -d "$directory" ]; then
+	mkdir "$directory"
+	echo "Directory created successfully: $directory"
+	
+else
+	echo "Directory already exists: $directory"
+fi
