eb03dbd8bb20ed088cca5d38ec52f473f89edce0
Don Aime <118371312+DUSHIME1212@users.noreply.github.com>
2024-02-21 13:06:41 +0200

    fix: making backup

---
+++ backup-Negpod1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# declare remote server details variables
+
+host="64293e56bc62.3a2627c1.alu-cod.online"
+username="64293e56bc62"
+password="328d3b338a4ced526c9a"
+remote_dir="/summative/1023-2024j"
