2f34c01b80d85723401309ae4819c4cf4d199c4c
Cynthia Mutie <c.mutie@alustudent.com>
2024-02-22 02:27:27 -0800

    Testing the function

--- negpod_1-q1/main.sh
+++ negpod_1-q1/main.sh
@@ -41,8 +41,22 @@ delete_student_record(){
 
 #Home
 #Updating student record by ID
-update_student_record()
-{}
+update_student_record(){
+
+    read -p "Enter your Student ID" update_ID
+    if [ -f students-list_1023.txt ] && [[ $(grep -c "$update_ID" students-list_1023.txt) -gt 0 ]]; then
+	    read -p "Student name:" student_name
+	    read -p "Student age:" student_age
+	    read -p "Student email:" student_email
+
+	    sed -i "/^$update_ID,/c\\$update_ID,$student_name,$student_age,$student_email" students-list_1023.txt
+
+	    echo "the user has been updated"
+
+    else
+	    echo "Student not found"
+    fi
+}
 
 
 
